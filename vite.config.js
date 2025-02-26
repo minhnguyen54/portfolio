9e52aed38f508395a3c344c6199842bcf2fd5b6e
minhnguyen54 <minhnguyen246@gmail.com>
2025-02-26 08:59:25 -0500

    Fix GitHub Pages deployment

--- vite.config.js
+++ vite.config.js
@@ -1,8 +1,7 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react-swc";
 
-// https://vite.dev/config/
 export default defineConfig({
-  base: "/portfolio/",
+  base: "/portfolio/", // Match this to your GitHub repository name
   plugins: [react()],
 });
